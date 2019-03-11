dd09eb2b0577b43f06e7331a37c117f1823167b2
Meghana_N <meghanangwd@gmail.com>
2019-03-11 21:25:53 +0530

    KH angular node creation code

---
+++ src/app/node/node.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core';
+import { FormService } from '../form.service';
+
+@Component({
+  selector: 'app-node',
+  templateUrl: './node.component.html',
+  styleUrls: ['./node.component.css']
+})
+export class NodeComponent implements OnInit {
+  openform: boolean;
+  openConcept: boolean;
+  openProperty: boolean;
+  value: any;
+  openIntent: boolean;
+  selectedLevel:string;
+  data:string;
+  field:any;
+
+  constructor(private formService:FormService) { }
+
+  ngOnInit() {
+    
+  }
+
+  onClickOpenForm(){
+    this.openform=true;  
+    }
+
+  onClickOpenConceptForm(){
+    this.openConcept=true;
+  } 
+
+  onClickOpenIntentForm(){
+    this.openIntent=true;
+  }
+
+  selected(){
+    this.data=this.selectedLevel;
+    this.field.property='';
+    this.fieldArray=null;
+  }
+  
+  showProprties(){
+    this.openProperty=true;
+  }
+
+
+  private fieldArray: Array<any> = [];
+  private newAttribute: any = {};
+
+  addFieldValue() {
+      this.fieldArray.push(this.newAttribute)
+      this.newAttribute = {};
+  }
+
+  deleteFieldValue(index) {
+    this.fieldArray.splice(index, 1);
+}
+
+
+addLastProperty(){
+  this.fieldArray.push(this.newAttribute);
+}
+
+
+createNode(form) {
+  delete form.newAttributeProperty;
+  delete form.newAttributeValue;
+  this.value = form; 
+  console.log(this.value);
+  this.formService.createNode(this.value)
+  .subscribe(
+    data => {
+      console.log("POST Request is successful ", data);},
+    error => {
+      console.log("Error", error);}
+  );
+}
+createIntent(form) {
+  delete form.newAttributeProperty;
+  delete form.newAttributeValue;
+  this.value = form; 
+  console.log(this.value);
+  this.formService.createNode(this.value)
+  .subscribe(
+    data => {
+      console.log("POST Request is successful ", data);},
+    error => {
+      console.log("Error", error);}
+  );
+}
+
+}
